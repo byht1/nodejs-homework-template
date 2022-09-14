717a37fd90d18e74b92f24cf22f5504bddc15c9d
byht1 <vitalikbyht1@gmail.com>
2022-09-14 21:18:19 +0300

    src

--- src/helpers/createError.js
+++ src/helpers/createError.js
@@ -6,7 +6,7 @@ const messages = {
   409: "Conflict",
 };
 
-const createError = (status, message = message[status]) => {
+const createError = (status, message = messages[status]) => {
   const error = new Error(message);
   error.status = status;
 
