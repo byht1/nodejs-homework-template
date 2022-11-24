72607047b85b4ed70f92f0ec7d359b822662cfc5
byht1 <vitalikbyht1@gmail.com>
2022-11-24 10:01:59 +0200

    Update login.test.js

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -6,7 +6,12 @@ require("dotenv").config();
 const { DB_TEST_HOST, PORT } = process.env;
 
 const app = require("../src/app");
-const { User } = require("../src/models/user");
+// const { User } = require("../src/models/user");
+
+const newUser = {
+  email: `${Date.now()}ihdpa@gmail.com`,
+  password: "11223344",
+};
 
 describe("test user routes", () => {
   let server;
@@ -22,25 +27,37 @@ describe("test user routes", () => {
 
   // Я так розумію що він повинен чистити базу але з ним не працює(з закоментованим кодом)
 
-  //   afterEach((done) => {
-  //     mongoose.connection.db.dropCollection(() => {
-  //       mongoose.connection.close(() => done());
-  //     });
-  //   });
+  afterEach(async () => {
+    await mongoose.connection.dropCollection();
+    await mongoose.connection.close();
+  });
 
-  test("test login route", async () => {
-    const newUser = {
-      email: "ihdpa@gmail.com",
-      password: "11223344",
-    };
+  test("test/SingUp", async () => {
+    const { body, statusCode } = await request(app)
+      .post("/api/users/signup")
+      .send(newUser);
 
-    await User.create(newUser);
+    expect(statusCode).toBe(201);
+    expect(body).toEqual(
+      expect.objectContaining({
+        password: expect.any(String),
+        email: expect.any(String),
+        subscription: expect.any(String),
+        avatarURL: expect.any(String),
+        _id: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      })
+    );
+  });
 
+  test("test/LogIn", async () => {
     const response = await request(app).post("/api/users/login").send(newUser);
     const {
+      statusCode,
       body: { token, user },
     } = response;
-    expect(response.statusCode).toBe(200);
+    expect(statusCode).toBe(200);
     expect(token).toBeDefined();
     expect(user).toEqual(
       expect.objectContaining({
@@ -49,4 +66,17 @@ describe("test user routes", () => {
       })
     );
   });
+
+  // test("Login test", async () => {
+  //   const mReq = { body: { email: "avatar@email.com", password: "avatar" } };
+  //   const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+  //   await loginController(mReq, mRes);
+
+  //   expect(mRes.status).toBeCalledWith(200);
+  //   expect(mRes.token).toEqual(expect.anything());
+  //   expect(mRes.user.email).toEqual(expect.not.stringContaining(expected));
+  //   expect(mRes.send.user.password).toEqual(
+  //     expect.not.stringContaining(expected)
+  //   );
+  // });
 });
